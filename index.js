31a341c487fc615a02f70b2b549fc3225549e3e5
Pablo Novaes <ppablo.0028922@gmail.com>
2025-04-26 15:50:06 -0300

    fix: remove unused log

--- index.js
+++ index.js
@@ -37,7 +37,6 @@ async function main() {
             repoIsInitialized = false
             console.log("")
         }
-        console.log(repoIsInitialized);
 
         const { commitType, message } = await inquirer.prompt([
             {
